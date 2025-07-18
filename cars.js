e818ad95a5dd852370ed90eaac38b95300b56881
mquality5j <mquality5j@gmail.com>
2025-07-18 13:09:44 +0200

    Create cars.js

---
+++ cars.js
@@ -0,0 +1,26 @@
+const cars = [
+  {
+    vin: "1HGCM82633A004352",
+    model: "Honda Accord 2003",
+    title: "Clean Title",
+    location: "CA - Los Angeles",
+    img: "car1.jpg",
+    link: "car1.html"
+  },
+  {
+    vin: "1C4RJFBG0FC625797",
+    model: "Jeep Grand Cherokee 2015",
+    title: "Salvage",
+    location: "TX - Dallas",
+    img: "car2.jpg",
+    link: "car2.html"
+  },
+  {
+    vin: "WDBUF56X78B278934",
+    model: "Mercedes E350 2008",
+    title: "Rebuilt",
+    location: "NY - Brooklyn",
+    img: "car3.jpg",
+    link: "car3.html"
+  }
+];
